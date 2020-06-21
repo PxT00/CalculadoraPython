e233737e6d582ee8a54c33acf074e5d9d83f42da
PxT00 <PxT00@users.noreply.github.com>
2020-06-20 21:35:22 -0300

    English update, and the option of repeat the program if you want

--- calc.py
+++ calc.py
@@ -1,21 +1,31 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
 
-op = input("Escolha uma operação matemática (+ ADIÇÃO) (- SUBTRAÇÃO) (* MULTIPLICAÇÃO) (/ DIVISÃO) (** POTENCIAÇÃO)")
+def main():
+ n1 = int(input("Type a number "))
+ n2 = int(input("Another one: "))
 
-soma = n1+n2
-sub = n1-n2
-multi = n1*n2
-div = n1/n2
-pot = n1**n2
+ op = input("Choose an operation (+ ADDITION) (- SUBTRACTION) (* MULTIPLICATION) (/ DIVISION) (** POTENTIATION) ")
 
-if op == "+":
-    print("A soma é: ",soma)
-elif op == "-":
-    print("A subtração é: ",sub)
-elif op == "*":
-    print("A multiplicação é: ",multi)
-elif op == "/":
-    print("A divisão é: ",div)
-elif op == "**":
-    print(n1,"elevado à", n2,"é",pot)
+ soma = n1+n2
+ sub = n1-n2
+ multi = n1*n2
+ div = n1/n2
+ pot = n1**n2
+
+ if op == "+":
+      print("The result is: ",soma)
+ elif op == "-":
+      print("The result is: ",sub)
+ elif op == "*":
+      print("The result is: ",multi)
+ elif op == "/":
+      print("The result is: ",div)
+ elif op == "**":
+      print(n1,"to", n2,"is",pot)
+
+while True:
+    main()
+    if input("Do you want to do another operation? (Y/N)").strip().upper() != 'Y':
+        print("See you next time!")
+        break
+
+
